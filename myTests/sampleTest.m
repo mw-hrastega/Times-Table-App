7ba5c2c68a2cb03b66c59745413a14a0593d588d
mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>
2025-03-04 15:28:37 -0500

    Update sampleTest.m

--- myTests/sampleTest.m
+++ myTests/sampleTest.m
@@ -6,17 +6,17 @@ function testA(testCase)
 verifyEqual(testCase,2+3,5)
 end
 
-%function testB(testCase)
-%verifyGreaterThan(testCase,13,42)
-%end
+function testB(testCase)
+verifyGreaterThan(testCase,13,42)
+end
 
 function testC(testCase)
 verifySubstring(testCase,"Hello World!","llo")
 end
 
-%function testD(testCase)
-%assumeTrue(testCase,1)
-%end
+function testD(testCase)
+assumeTrue(testCase,1)
+end
 
 function testE(testCase)
 assertSize(testCase,ones(2,5,3),[2 5 3])
