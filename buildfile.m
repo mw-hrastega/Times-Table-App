8515d304406c98488d3eccf34aeaadfa12c02ad1
mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>
2025-03-04 16:08:17 -0500

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -9,7 +9,7 @@ plan = buildplan(localfunctions);
 plan("check") = CodeIssuesTask;
 
 % Add a task to run tests
-plan("test") = TestTask("tests");
+plan("test") = TestTask("myTests");
 
 % Make the "archive" task the default task in the plan
 plan.DefaultTasks = "archive";
