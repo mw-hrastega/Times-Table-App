3f64223922e211b24b8d93cc86156a4692e0e7b3
mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>
2025-03-04 09:40:35 -0500

    Add files via upload

---
+++ myTests/sampleTest.m
@@ -0,0 +1,23 @@
+function tests = sampleTest
+tests = functiontests(localfunctions);
+end
+
+function testA(testCase)
+verifyEqual(testCase,2+3,5)
+end
+
+function testB(testCase)
+verifyGreaterThan(testCase,13,42)
+end
+
+function testC(testCase)
+verifySubstring(testCase,"Hello World!","llo")
+end
+
+function testD(testCase)
+assumeTrue(testCase,1)
+end
+
+function testE(testCase)
+assertSize(testCase,ones(2,5,3),[2 5 3])
+end
